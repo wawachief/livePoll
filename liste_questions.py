d7bb89be9084316443f049884a570dd2efa5d28d
Olivier Lecluse <olivier.lecluse@free.fr>
2020-05-02 14:27:21 +0200

    Security, qrcode and ajax

---
+++ liste_questions.py
@@ -0,0 +1,19 @@
+questions = {
+    # No : [ Question, [(code, reponse), ( , )...], QCU?QCM]
+    # False : QCM / True : QCU
+    1 : ["Question Vrai / Faux", 
+		["Vrai", "Faux"],
+		True],
+	2 : ["Question Oui / Non", 
+		["Oui", "Non"],
+		True],
+	3 : ["Choix unique A B C D", 
+		["Réponse A", "Réponse B", "Réponse C", "Réponse D"],
+		True],
+	4 : ["Choix multiple QCM A B C D", 
+		["Réponse A", "Réponse B", "Réponse C", "Réponse D"],
+		False]
+}
+
+if __name__ == '__main__':
+    pass
--- views.py
+++ views.py
@@ -2,31 +2,48 @@
 # Mai 2020
 # Licence GPL-3.0-or-later
 
-from flask import Flask, render_template, request, Markup, redirect, url_for
+from flask import Flask, render_template, request, Markup, redirect, url_for, send_file
+from requests import get
+import io
+import qrcode
+from liste_questions import questions
 
 app = Flask(__name__)
 
-questions = {
-    # No : [ Question, [(code, reponse), ( , )...], QCU?QCM]
-    # False : QCM / True : QCU
-    1 : ["Question Vrai / Faux", 
-		["Vrai", "Faux"],
-		True],
-	2 : ["Question Oui / Non", 
-		["Oui", "Non"],
-		True],
-	3 : ["Choix unique A B C D", 
-		["Réponse A", "Réponse B", "Réponse C", "Réponse D"],
-		True],
-	4 : ["Choix multiple QCM A B C D", 
-		["Réponse A", "Réponse B", "Réponse C", "Réponse D"],
-		False]
-}
-
 reponses = dict()
+
+# Parametrage IP
+# Recuperer l'IP publique automatiquement
+ip_pub = get('https://api.ipify.org').text
+port_pub = 5000 # port public
+# machines autorisees pour administration
+ip_admin = ["127.0.0.1"]
 
 q_en_cours = 0
 new_quest = False
+# Affichage blocs supplementaires dans bilan
+quest_enable = True  # Rappel question dans bilan
+qrcode_enable = True
+
+#
+# Fabrication QRCode
+#
+
+@app.route('/qrcode.png')
+def qrcode_png():
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=5,
+        border=2,
+    )
+    qr.add_data(f"http://{ip_pub}:{port_pub}/")
+    qr.make(fit=True)
+    output = io.BytesIO()
+    qrcode_img = qr.make_image(fill_color="black", back_color="white")
+    qrcode_img.get_image().save(output, format='PNG')
+    output.seek(0, 0)
+    return send_file(output, mimetype='image/png', as_attachment=False)
 
 def ajoute_reponse(q, r):
     """Ajoute une reponse r à la question q"""
@@ -53,6 +70,11 @@ def index():
 
 @app.route('/admin.html')
 def admin():
+    # Securisation de la page
+    host = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    if host not in ip_admin:
+        return 
+
     listquest = '<input type="radio" name="n_quest" value="0" id="rep0" /> <label for="rep0">Page d\'attente</label><br />'
     liste_no_questions = list(questions.keys())
     liste_no_questions.sort()
@@ -66,6 +88,11 @@ def admin():
 @app.route('/choixrep.html', methods=["POST"])
 def choixrep():
     global q_en_cours, new_quest
+    # Securisation de la page
+    host = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    if host not in ip_admin:
+        return 
+        
     reponse = request.form
     q_en_cours = int(reponse["n_quest"])
     new_quest = True
@@ -108,52 +135,109 @@ def reponse():
 # Bilans 
 #
 
-@app.route('/bilan1', methods=["GET"])
-@app.route('/bilan', methods=["GET"])
-def bilan():
-    """Bilan final avec scores """
+def recupere_params_get(req):
     try:
-        n_quest_form = request.args
-        n_quest = int(n_quest_form["q"])
+        n_quest_form = req.args
+        n_quest = int(n_quest_form.get("q",1))
     except:
         n_quest = 1
-    # Reponses masquees
-    hidden =  "bilan1" in str(request.url)
-    refresh_cmd = '<meta http-equiv="refresh" content="3" />' if hidden else ''
+        hidden = 1
+    else:
+        hidden = int(n_quest_form.get("hidden",1))
+    return n_quest, hidden
 
+@app.route('/bilan_list', methods=["GET"])
+def bilan_list():
+    """Appel ajax bilan
+        appel avec parametres get
+        - q : no de question
+        - hidden (0 ou 1) intitulkes masques"""
+
+    # Securisation de la page
+    host = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    if host not in ip_admin:
+        return 
+
+    n_quest, hidden = recupere_params_get(request)
+    
+    # Liste des reponses 
     if n_quest in reponses:
         replist = []
         bilstr = '<ul>'
         for (nq,q) in enumerate(questions[n_quest][1]):
             nb_reponses = reponses[n_quest].get(nq, 0)
-            if hidden : 
+            if hidden == 1: 
                 replist.append(nb_reponses)
             else:
                 bilstr += f"<li>{q} : {nb_reponses}</li>"
-        if hidden:
+        if hidden == 1:
             replist.sort()
             for r in replist:
                 bilstr += f"<li>{r}</li>"
-                questionstr = questions[n_quest][0]
         bilstr += '</ul>'
-        questionstr = questions[n_quest][0]
     else:
         bilstr = "<p>Pas de réponses encore à la question</p>"
-        questionstr = questions[n_quest][0] if n_quest in questions else "Plus de question !!"
-    return render_template('bilan.html', q=n_quest, question=questionstr, 
-        resultats=Markup(bilstr), refresh=Markup(refresh_cmd))
+    return Markup(bilstr)
+    
+@app.route('/bilan', methods=["GET"])
+def bilan():
+    """Bilan final avec scores """
 
+    # Securisation de la page
+    host = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    if host not in ip_admin:
+        return 
+
+    # Recuperation no question
+    n_quest, hidden = recupere_params_get(request)
+    
+    questionstr = questions[n_quest][0] if n_quest in questions else "Plus de question !!"
+
+    # Rappel de la question
+    quest_html = ""
+    if quest_enable and n_quest in questions:
+        quest_html = f"<ul>"
+        for r in questions[n_quest][1]:
+            quest_html += f"<li>{r}</li>"
+        quest_html += "</ul>"
+    
+    # Affichage qrcode
+    qrcode_html = ""
+    if qrcode_enable:
+        qrcode_html = f"<img src=\"{url_for('qrcode_png')}\" class=\"qrcode\" >"
+    return render_template('bilan.html', q = n_quest, question = questionstr,
+        qrcode = Markup(qrcode_html), quest = Markup(quest_html), hide = hidden)
+
+
+@app.route('/bilan_reptoggle', methods=["GET"])
+@app.route('/bilan_qrtoggle', methods=["GET"])
 @app.route('/bilan2', methods=["GET"])
 def bilan2():
-    """Bilan intermediaire avec reponses masquees"""
-    try:
-        n_quest_form = request.args
-        n_quest = int(n_quest_form["q"])
-    except:
-        n_quest = 0
-    if n_quest in reponses:
+    """Reinitialisation du bilan"""
+    global qrcode_enable, quest_enable
+
+    # Securisation de la page
+    host = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    if host not in ip_admin:
+        return 
+
+    bilan2 =  "bilan2" in str(request.url)
+    qr =  "qrtoggle"   in str(request.url)
+    rep =  "reptoggle" in str(request.url)
+    
+    n_quest, _ = recupere_params_get(request)
+
+    if qr:
+        qrcode_enable = not qrcode_enable
+    if rep:
+        quest_enable = not quest_enable
+    if bilan2 and n_quest in reponses:
         reponses[n_quest].clear()
-    return redirect(url_for('bilan') + f"1?q={n_quest}")
+    return redirect(url_for('bilan') + f"?q={n_quest}&hidden=1")
+
+#
+# Lancement de l'application
+#
 
 app.run(host= '0.0.0.0')
 
